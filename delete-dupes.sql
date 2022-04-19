36559be04a5ec6c93540b9a640cc11e2a132052c
Sara Sun <29992487+sarasun-mse@users.noreply.github.com>
2022-04-19 14:11:42 -0400

    Create delete-dupes.sql

---
+++ delete-dupes.sql
@@ -0,0 +1,9 @@
+/*
+example removing duplicates with some conditions
+specifically one col has dupes and 
+another col defines which ones to remove or keep
+two approaches:
+one by tbl self join
+the other by 
+*/
+
